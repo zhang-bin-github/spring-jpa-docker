273c8bfe1a387495e7b7774089cfa9dc4e6ddf55
zhang-bin-github <839391223@qq.com>
2020-05-15 12:27:12 +0800

    test test

---
+++ src/test/java/com/momo/springboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momo.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
