fd6070cb4abd15774db511ebf9c8d7fae2e13e85
zhang-bin-github <839391223@qq.com>
2020-05-15 13:13:32 +0800

    test test

--- src/main/java/com/momo/springboot/DemoApplication.java
+++ src/main/java/com/momo/springboot/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("这是第一个上传至github的项目 omg！");
         System.out.println("这是第一个上传至github的项目 omg！");
+        System.out.println("这是第一个上传至github的项目 omg！22");
+        System.out.println("这是第一个上传至github的项目 omg！33");
         SpringApplication.run(DemoApplication.class, args);
     }
 
